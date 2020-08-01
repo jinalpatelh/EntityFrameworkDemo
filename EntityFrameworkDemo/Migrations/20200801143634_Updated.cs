8aab184794dba27697fa5cc60acbbd265f37203a
girlwhocode <jinalpatelh@gmail.com>
2020-08-01 11:38:05 -0400

    Add project files.

---
+++ EntityFrameworkDemo/Migrations/20200801143634_Updated.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkDemo.Migrations
+{
+    public partial class Updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Teachers_TeacherId",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeacherId",
+                table: "Courses",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Teachers_TeacherId",
+                table: "Courses",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_Teachers_TeacherId",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeacherId",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_Teachers_TeacherId",
+                table: "Courses",
+                column: "TeacherId",
+                principalTable: "Teachers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+
+        public class SchoolContext : DbContext
+        {
+
+            public DbSet<StudentPoco> Students { get; set; }
+            public DbSet<CoursePoco> Courses { get; set; }
+            public DbSet<TeacherPoco> Teachers { get; set; }
+            public DbSet<MarkPoco> Marks { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-DTULATU7\HUMBERBRIDGING;Initial Catalog=HUMBER_MARKS_DB;Integrated Security=True;");
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<MarkPoco>()
+                    .HasKey(e => new { e.CourseId, e.StudentId });
+                
+                modelBuilder.Entity<StudentCoursePoco>()
+                    .HasKey(e => new { e.CourseId, e.StudentId });
+
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+        public class StudentPoco
+        {
+            public int Id { get; set; }
+            
+            [MaxLength(50)]
+            public string Name { get; set; }
+            public int CourseId { get; set; }
+            public List<StudentCoursePoco> StudentCourse { get; set; }
+            
+        }
+        public class CoursePoco
+        {
+            public int Id { get; set; }
+            
+            [MaxLength(50)]
+            public string Name { get; set; }
+            public TeacherPoco Teacher { get; set; }
+            public List<StudentCoursePoco> StudentCourse { get; set; }
+
+        }
+        public class TeacherPoco
+        {
+            [Key]
+            public int Id { get; set; }
+            [MaxLength(50)]
+            public string Name { get; set; }
+            public int CourseId { get; set; }
+            public List<CoursePoco> Course { get; set; }
+        }
+        public class MarkPoco
+        {
+            public int CourseId { get; set; }
+            public int StudentId { get; set; }
+            public double Mark { get; set; }
+            public double MaxMark { get; set; }
+            public CoursePoco Course { get; set; }
+            public StudentPoco Student { get; set; }
+        }
+
+        public class StudentCoursePoco
+        {
+            public int StudentId { get; set; }
+            public int CourseId { get; set; }
+            public StudentPoco Student { get; set; }
+            public CoursePoco Course { get; set; }
+        }
+
+    }
+}
